89296acb92687deaaa338539666fd212656c6fac
Mykola Nakonechnyi <m.v.m.nak@gmail.com>
2020-02-06 15:14:56 +0200

    add HW

--- HW.1Les.py
+++ HW.1Les.py
@@ -1,9 +1,13 @@
+#примітивний калькулятор
+
 a = int(input("a="))
 b = int(input("b="))
 print("a+b=", a+b)
 print("a-b=", a-b)
 print("a*b=", a*b)
 print("a/b=", a/b)
+
+#штучний ітелект гадалки)
 
 name = input("What is your name?")
 age = input("How old are you?")
